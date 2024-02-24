ac8bb857fa19aaee91ab3d7830c805052f45dfe6
Prasun Mondal <prasun0403.pm@gmail.com>
2024-02-24 10:44:03 +0530

    loder updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <>
-      <div className="w-full h-auto flex justify-center items-center">
+      <div className="w-full h-auto flex justify-center items-center col-span-12">
         <div className="loader"></div>
       </div>
     </>
--- src/components/TrendSuggestion.jsx
+++ src/components/TrendSuggestion.jsx
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from "react";
-import Image from "./img/Home.jpg";
-import Freg from "./img/freg.jpg";
-import Grosery from "./img/grosery.jpg";
-import HomeDecore from "./img/home-decore.jpg";
-import Laptop from "./img/laptop.jpg";
-import Phone from "./img/phone.jpg";
-import Skin from "./img/skin.jpg";
+
 import Loader from "./Loader";
 import { Link } from "react-router-dom";
 
 export default function TrendSuggestion() {
-  // const CatagoryImages = [
-  //   Freg, Grosery, HomeDecore, Laptop, Phone, Skin
-  // ];
+  const popularSuggestion = [
+    {
+      id: 0,
+      name: "Striped shirt dress"
+    },
+    {
+      id: 1,
+      name: "Satin shirts"
+    },
+    {
+      id: 2,
+      name: "Denim jumpsuit"
+    },
+    {
+      id: 3,
+      name: "Leather dresses"
+    },
+    {
+      id: 4,
+      name: "Solid tshirts"
+    }
+    
+  ]
   const [catagories, setCatagories] = useState([]);
-  const [loader, setLoader] = useState(false);
+  const [loader, setLoader] = useState(true);
+  const [popularSuggestions, setPopularSuggestions] = useState(popularSuggestion);
 
-  // console.log(catagories);
+  // console.log(popularSuggestions);
   const fetchData = async () => {
     setLoader(true);
     try {
@@ -34,17 +49,22 @@ export default function TrendSuggestion() {
     setLoader(false);
   };
 
+  
   useEffect(() => {
     fetchData();
-    // console.log(catagories);
+    const interval = setInterval(popularSuggestion, 3000);
+    // return clearInterval(interval)
+
+    console.log(interval);
   }, []);
 
   return (
     <>
-      <div className="w-full flex flex-col lg:gap-4 gap-2 h-auto bg-white px-[1.25rem] lg:px-[2rem] py-[1.7rem] rounded-[.3rem] ">
+      <div className="w-full flex flex-col lg:gap-4 gap-2 h-auto bg-white px-[1.25rem] lg:px-[2rem] py-[1.7rem] rounded-[.3rem] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
         <h2 className="text-[#000000] text-[2rem] ">Latest Trend</h2>
         <div className="w-full h-auto grid grid-cols-12 gap-4">
           <div className="hidden lg:block col-span-1"></div>
+          
           {loader ? (
             <Loader />
           ) : (
@@ -52,13 +72,13 @@ export default function TrendSuggestion() {
             catagories.slice(0, 5).map((item, id) => {
               return (
                 <Link
-                  className="w-full h-auto lg:col-span-2 sm:col-span-6 md:col-span-4 col-span-12  border1"
+                  className="w-full h-auto lg:col-span-2 sm:col-span-6 md:col-span-4 col-span-12 rounded-[0.25rem] "
                   key={id}
                   to={`/product-page/${id +1}/products`}
                 >
                   <div className="w-full h-auto flex flex-col ">
-                    <img src={item.image} alt="" />
-                    <h4 className="text-[1.3rem] text-center">{item.name}</h4>
+                    <img src={item.image} alt="" className="rounded-[0.25rem] sm:mb-[0.625rem] mb-[0.425rem] "/>
+                    <p className="text-[1.3rem] text-left">{item.name}</p>
                   </div>
                 </Link>
               );
